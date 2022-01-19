2d46163d17088f7c460989bbfe4abc1b78a509ca
Joseph Essel Moses <jmosesessel21@gmail.com>
2022-01-19 19:51:39 +0000

    added sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="flex w-full min-h-screen font-sans bg-gray-100">
-    
+    <Sidebar />
+    <main>
+      
+    </main>
   </div>
 </template>
   
+<script setup>
+import Sidebar from './components/Sidebar.vue';
+
+</script>

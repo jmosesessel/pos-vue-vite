b08e638214ac691fea3e1cdb96b3c4d31dbba295
Joseph Essel Moses <jmosesessel21@gmail.com>
2022-01-19 20:28:01 +0000

    renamed Sidebar.vue to SideBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="flex w-full min-h-screen font-sans bg-gray-100">
-    <Sidebar />
+    <SideBar />
     <main>
       
     </main>
+    <aside class="flex flex-col gap-y6 pt-6 pr-6 w-96">
+      <RightSideBar />
+    </aside>
   </div>
 </template>
   
 <script setup>
-import Sidebar from './components/Sidebar.vue';
-
+import SideBar from './components/SideBar.vue'
+import RightSideBar from './components/RightSideBar.vue';
 </script>

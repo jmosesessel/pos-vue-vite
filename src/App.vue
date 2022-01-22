4280b272085d78e394cd08c6b2a81568d281b7fe
Joseph Essel Moses <jmosesessel21@gmail.com>
2022-01-22 01:00:37 +0000

    completed RigtSideBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="flex w-full min-h-screen font-sans bg-gray-100">
     <SideBar />
-    <main>
-      
+    <main class="flex flex-col flex-1">
+      <header>
+        <h1 class="text-x3"></h1>
+      </header>
     </main>
     <aside class="flex flex-col gap-y6 pt-6 pr-6 w-96">
       <RightSideBar />

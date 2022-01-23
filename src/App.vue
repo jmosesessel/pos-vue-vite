d2d2767f4b8d5cc5a2a84d66d500723d65d88b8c
Joseph Essel Moses <jmosesessel21@gmail.com>
2022-01-23 01:03:33 +0000

    completed Card.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="flex w-full min-h-screen font-sans bg-gray-100">
     <SideBar />
-    <main class="flex flex-col flex-1">
+    <main class="flex flex-col flex-1 gap-3 p-4">
       <header>
-        <h1 class="text-x3"></h1>
+        <h1 class="text-x3 font-semibold leading-loose text-gray-70">POS System</h1>
       </header>
+      <hr class="border-red-700" > 
+      <Card />
     </main>
     <aside class="flex flex-col gap-y6 pt-6 pr-6 w-96">
       <RightSideBar />
@@ -15,4 +17,5 @@
 <script setup>
 import SideBar from './components/SideBar.vue'
 import RightSideBar from './components/RightSideBar.vue';
+import Card from './components/Card.vue';
 </script>
